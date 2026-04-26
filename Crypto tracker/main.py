61e4316b3059e70503bb647e4979ba4c27cdd757
Danylo-def <antonomelcenko45@gmail.com>
2026-04-26 14:01:58 +0300

    crypto tracker with API working

--- Crypto tracker/main.py
+++ Crypto tracker/main.py
@@ -3,39 +3,47 @@
 #python 3.13.3
 #pip install requests
 
-
+import requests
 from datetime import datetime, timedelta #connect datetime
 
 file = open('Crypto tracker/History.txt', 'a')# file open
 
+while True:
+
 # console input
-ques = input ("What coin you want to check? :  ").upper()#input with upper method 
+    ques = input ("What coin you want to check? :  ").upper()#input with upper method 
+
+    if ques == "EXIT":
+        break
 
 #dictionary
-price = {
-"ETH": 2300,
-"BTC": 75000,
-"SOL": 85
+    coin_map = {
+"ETH": "ethereum",
+"BTC": "bitcoin",
+"SOL": "solana",
+"ADA": "cardano"
 }
-#condition to check, have or not coin in the dictionary
-if ques in price:
-    price_value = price[ques]
-    #continue the programm
-else: 
-    print("Coin not Found")
-    exit()
+#condition to check, if no coin in dic.
+    if ques not in coin_map:
+        print("Coin not Found")
+        exit()
 
+    coin_id = coin_map[ques]
 
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
+
+    response = requests.get(url)
+    data = response.json()
 
 #File outcome
-selected_coin = (f"You selected : {ques}") 
-price_value =(f"The Price is : {price[ques]}")
-time_shown = (f"Time: {datetime.now().strftime('%Y %d %b %H:%M:%S')}")
+    selected_coin = (f"You selected : {ques}") 
+    price_value = f"{ques} Price: {data[coin_id]['usd']} USD"
+    time_shown = (f"Time: {datetime.now().strftime('%Y %d %b %H:%M:%S')}")
 
 #Output variables in file
-whole = (f"\n{ques} \n {selected_coin} \n {price_value} \n {time_shown}")
-print(whole)
-file.write(whole + '\n')
+    whole = (f"\n{ques} \n {selected_coin} \n {price_value} \n {time_shown}")
+    print(whole)
+    file.write(whole + '\n')
 
 
-file.close()# close file 
+    file.close()# close file 
