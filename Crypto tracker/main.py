abd78a879055603c2fc8df08800aa17688152632
Danylo-def <antonomelcenko45@gmail.com>
2026-05-05 21:22:53 +0300

    added design

--- Crypto tracker/main.py
+++ Crypto tracker/main.py
@@ -64,20 +64,21 @@ while True: #loop to not rerun code
 
 
 #File outcome
-        selected_coin = (f"You selected : {ques}") 
-        price_value = f"{ques} Price: {price} USD"
-        time_shown = (f"Time: {datetime.now().strftime('%Y %d %b %H:%M:%S')}")
+        name_project = ("\t CRYPTO TRACKER").upper()
+        selected_coin = (f"Coin  : {ques}") 
+        price_value = f"Price : {price} USD"
+        time_shown = (f"Time  : {datetime.now().strftime('%Y %d %b %H:%M:%S')}")
 
 #Output variables in file
-        whole = (f"\n{ques} \n {selected_coin} \n {price_value} \n {time_shown}")
+        whole = (f"{"="* 30} \n {name_project} \n {"="* 30} \n\n {selected_coin} \n {price_value} \n {time_shown}")
 
     #creating live-terminal
         os.system('cls' if os.name == 'nt'else 'clear')# help to get it what u use (windows,mac or linux)
-        print(whole)
-        print("\n Type CTRL+C to stop")# w\i
-        
-        
+        print(f"{whole}\n")
+        whole_2 = (f"{"-"* 30}\n {'Type CTRL+C to stop'} \n {"="*30}")
+        print(whole_2)
+        #write in file
         with open('Crypto tracker/History.txt', 'a') as file:
-            file.write(whole + '\n')
+            file.write(f"{whole}\n {whole_2} \n")
 
         time.sleep(15)# it will spawn new block after 15sec. w\i
